cc0d732b5ddee859aab5b094498d08a82101cb4c
lufa1217 <nek_noke@yahoo.co.jp>
2016-02-07 16:01:41 +0900

    mvn_init
    初回コミット

---
+++ src/main/java/consumer/mermaid/App.java
@@ -0,0 +1,24 @@
+package consumer.mermaid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+    public boolean test(int i){
+
+    	if(i%2 ==0){
+    		return true;
+    	}else{
+    		return false;
+    	}
+
+    }
+}
