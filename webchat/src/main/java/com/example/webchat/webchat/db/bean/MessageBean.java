26f1c8d7dd58b0fc02c1c364d459ad87f20427cf
lufa1217 <nek_noke@yahoo.co.jp>
2016-03-06 15:04:08 +0900

    ittannkrnyou2

--- webchat/src/main/java/com/example/webchat/webchat/db/bean/MessageBean.java
+++ webchat/src/main/java/com/example/webchat/webchat/db/bean/MessageBean.java
@@ -15,4 +15,60 @@ public class MessageBean {
     //
     //  ... コンストラクタ及びアクセッサ.
     //
+
+    public MessageBean(){
+
+    }
+
+    public MessageBean(Date date ,String name ,String message){
+    	this.date = date;
+    	this.name = name;
+    	this.message = message;
+    }
+
+	/**
+	 * @return date
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * @param date セットする date
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	/**
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name セットする name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message セットする message
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+
+
 }
