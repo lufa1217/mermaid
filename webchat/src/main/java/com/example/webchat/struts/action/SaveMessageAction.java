5ef5ab18f343de26aba206c0e568cd6d57010edb
lufa1217 <nek_noke@yahoo.co.jp>
2016-03-06 15:04:28 +0900

    ittannkanryou3

---
+++ webchat/src/main/java/com/example/webchat/struts/action/SaveMessageAction.java
@@ -0,0 +1,35 @@
+package struts.action;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.DynaActionForm;
+
+import webchat.db.bean.MessageBean;
+import webchat.db.dao.MessageDao;
+import webchat.db.dao.impl.MessageDaoOnMemoryImpl;
+
+
+public class SaveMessageAction {
+    public ActionForward execute(
+            ActionMapping mapping, ActionForm actionForm,
+            HttpServletRequest request, HttpServletResponse response)
+    {
+        DynaActionForm form = (DynaActionForm)actionForm;
+        Date       date       = new Date();
+        String     name       = form.getString("name");
+        String     message    = form.getString("message");
+        MessageDao messageDao = MessageDaoOnMemoryImpl.getInstance();
+        messageDao.save(new MessageBean(date, name, message));
+
+        // 発言フォームの「名前」の値はそのまま残し, 「メッセージ」の値はクリアする.
+        form.set("message", "");
+
+        return mapping.findForward("success");
+    }
+}
---
+++ webchat/src/main/java/com/example/webchat/struts/action/ShowMessagesAction.java
@@ -0,0 +1,20 @@
+package struts.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import webchat.db.dao.MessageDao;
+import webchat.db.dao.impl.MessageDaoOnMemoryImpl;
+
+public class ShowMessagesAction {
+    public ActionForward execute(ActionMapping mapping, ActionForm form,HttpServletRequest request, HttpServletResponse response)
+    {
+        MessageDao messageDao = MessageDaoOnMemoryImpl.getInstance();
+        request.setAttribute("messages", messageDao.getAll());
+        return mapping.findForward("success");
+    }
+}
